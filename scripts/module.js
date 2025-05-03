2aa7a470844b09d1265c16ab3b0ea7251f374483
Ian Guswiler <iangus97@gmail.com>
2025-05-03 10:41:47 -0400

    hello world

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,13 @@
-Hooks.once('init', async function() {
+console.log("Hello World! This code runs immediately when the file is loaded.");
 
+Hooks.on("init", function () {
+  console.log(
+    "This code runs once the Foundry VTT software begins its initialization workflow."
+  );
 });
 
-Hooks.once('ready', async function() {
-
+Hooks.on("ready", function () {
+  console.log(
+    "This code runs once core initialization is ready and game data is available."
+  );
 });
