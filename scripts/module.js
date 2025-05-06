cf58061b17fad44faa0fd936d3fa72b8900c3979
Ian Guswiler <iangus97@gmail.com>
2025-05-06 12:43:30 -0400

    remove todos

--- scripts/module.js
+++ scripts/module.js
@@ -60,7 +60,6 @@ Hooks.on("deleteScene", (scene) => {
   dataStoresMap.delete(scene.id);
 });
 
-// TODO Use libWrapper to override Token._canDrag based on configured scene setting
 // TODO figure out how to modify how much a token moves when using arrow keys
 // TODO override grid.getShiftedPoint to calculate the movement relative to grid size
 // TODO allow movement outside grid-snapping? is this how gridless works already? Look into why gridless can sometimes move the player into wierd positions
